4372f11fc37ec849cb18374cff8f488224331cd9
Georg Bernold <bernold.georg@gmail.com>
2019-01-26 16:15:51 +0100

    rename visualization

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export default function (kibana) {
   return new kibana.Plugin({
     uiExports: {
       visTypes: [
-        'plugins/preha_treemap_visualization/treemap_vis'
+        'plugins/kibana_treemap_visualization/treemap_vis'
       ],
       styleSheetPaths: `${__dirname}/public/index.scss`,
     }
